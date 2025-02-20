b28e97c5a0d2fa3f2ad0f3193a128281db31094e
sukori <sukori98@gmail.com>
2025-02-20 09:22:13 +0100

    init srcs

---
+++ Sources/src/miniRT.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniRT.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 08:55:48 by pberset           #+#    #+#             */
+/*   Updated: 2025/02/20 09:20:44 by pberset          ###   Lausanne.ch       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/miniRT.h"
+
+int	main(int argc, char *argv[])
+{
+	ft_printf("%d Hello %s World\n", argc, argv[0]);
+	return (0);
+}
