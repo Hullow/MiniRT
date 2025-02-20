62033060bb7e91a848a6c99557ec19ca0b96715b
pberset <pberset@student.42lausanne.ch>
2025-02-20 11:19:51 +0100

    mlx compile

--- Sources/src/miniRT.c
+++ Sources/src/miniRT.c
@@ -6,7 +6,7 @@
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 08:55:48 by pberset           #+#    #+#             */
-/*   Updated: 2025/02/20 09:20:44 by pberset          ###   Lausanne.ch       */
+/*   Updated: 2025/02/20 11:13:16 by pberset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 
 int	main(int argc, char *argv[])
 {
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 100, 100, "miniRT");
+	mlx_loop(mlx);
+	(void)mlx_win;
+	//mlx_destroy_window(mlx, mlx_win);
+	//mlx_destroy_display(mlx);
 	ft_printf("%d Hello %s World\n", argc, argv[0]);
 	return (0);
 }
