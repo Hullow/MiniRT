58e03d5c71bacb3b8d75c8ecae10a9ac9278f820
pberset <pberset@student.42lausanne.ch>
2025-02-21 17:38:32 +0100

    scene

--- Sources/src/miniRT.c
+++ Sources/src/miniRT.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   miniRT.c                                           :+:      :+:    :+:   */
+/*   miniRT.c                                            :+:    :+:           */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pberset <pberset@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 08:55:48 by pberset           #+#    #+#             */
-/*   Updated: 2025/02/21 13:57:20 by pberset          ###   ########.fr       */
+/*   Updated: 2025/02/21 17:38:05 by pberset        ########   odam.nl        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(int argc, char *argv[])
 {
+	t_scene	scene;
+
 	if (argc != 2)
 	{
 		ft_puterr_fd("error: one <file>.rt expected\n");
@@ -21,7 +23,8 @@ int	main(int argc, char *argv[])
 	}
 	if (rt_check_ext(argv[1]))
 		return (2);
-	if (rt_extract_scene(argv[1]))
+	scene = rt_extract_scene(argv[1]);
+	if (!scene)
 		return (3);
 	if (rt_check_scene(argv[1]))
 		return (4);
