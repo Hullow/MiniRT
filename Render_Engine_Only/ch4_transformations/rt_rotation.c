31cad23d9ae97890de8a6bc3ebf84b132851d0a6
fallan <fallan@student.42lausanne.ch>
2025-06-05 17:37:59 +0200

    Rotation: fit the function to norm and added function docstring

--- Render_Engine_Only/ch4_transformations/rt_rotation.c
+++ Render_Engine_Only/ch4_transformations/rt_rotation.c
@@ -6,14 +6,22 @@
 /*   By: fallan <fallan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 20:26:27 by pberset           #+#    #+#             */
-/*   Updated: 2025/06/05 17:27:22 by fallan           ###   ########.fr       */
+/*   Updated: 2025/06/05 17:36:59 by fallan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-// Init a rotation matrix around the 3 axis
-// Takes in the object's normal vector
+/// @brief Init a rotation matrix that rotates an object to have norm "norm"
+/// @details the norm is our desired Y-axis, so it keeps it unchanged.
+/// it checks if the norm is parallel to the X-axis using the magnitude of the
+/// their cross product:
+/// - If it is not, it uses the X-basis vector (1, 0, 0)
+/// as the second basis vector, "new_x".
+/// - If it is, it uses the Z-basis vector (0,0,1).
+/// The third new basis vector "new_z "is calculated using 
+/// the cross product of the first two "new_y" and "new_x".
+/// The vector components are used to init the corresponding rotation matrix.
 t_matrix	rt_rotation(t_tuple norm)
 {
 	t_matrix	rotate;
@@ -21,10 +29,11 @@ t_matrix	rt_rotation(t_tuple norm)
 	t_tuple		new_y;
 	t_tuple		new_z;
 
-	new_y = norm;// the norm is our desired Y-AXIS
-	if (is_equal_float(0, rt_magnitude(rt_cross_product(new_y, rt_vector(1, 0, 0))))) // if vectors are parallel
+	new_y = norm;
+	if (is_equal_float(
+			0, rt_magnitude(rt_cross_product(new_y, rt_vector(1, 0, 0)))))
 		new_x = rt_normalize(rt_cross_product(new_y, rt_vector(0, 0, 1)));
-	else 
+	else
 		new_x = rt_normalize(rt_cross_product(new_y, rt_vector(1, 0, 0)));
 	new_z = rt_cross_product(new_x, new_y);
 	rotate = rt_identity_matrix(4);
